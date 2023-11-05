fd981049fd3d9f800fc377c7bf1262cf7a981be8
CuriousChum <109543738+CuriousChum@users.noreply.github.com>
2023-11-05 01:02:23 -0500

    finished the overview for the front page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 export default function Navbar() {
    return (
-       <div className="navbar bg-base-100 bg-blue-700">
+       <div className="navbar bg-base-100 bg-blue-500">
            <div className="flex-1">
                <a className="btn btn-ghost normal-case text-xl">VenueLoca</a>
            </div>
-           <div className="flex-none">
-               <button className="btn btn-square btn-ghost">
-                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+           <div className="flex-end">
+                <button className='flex-end'>
+                    <a className="btn btn-ghost normal-case">
+                        Log In
+                    </a>
+               </button>
+               <button className='flex-end'>
+                    <a className="btn btn-ghost normal-case">
+                        Register
+                    </a>
                </button>
            </div>
        </div>
--- src/components/VenueCollection.jsx
+++ src/components/VenueCollection.jsx
@@ -22,12 +22,15 @@ export default function VenueCollection() {
     }, [publicRuntimeConfig.API_URL])
 
     return (
-        <div>
-            <div className="text-3xl font-bold">
-                Venue List
+        <div className="p-5 ">
+            <div className="bg-slate-100">
+            <div className="navbar bg-base-auto bg-blue-500 rounded-t-lg outline">
+                <div className="flex-1">
+                    <a className="pl-3 font-bold text-2xl text-white">Venue Lists</a>
+                </div>
             </div>
-            <div>
-                {venues && <ul className="w-full min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-2 p-[4%]">
+                <div class="bg-base-100 outline rounded-b-lg">
+                {venues && <ul className="w-full min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-2 p-[2%]">
                     {venues.map((item) => (
                         <li key={item.id} className="w-full p-[2%]">
                             <VenueCard
@@ -36,10 +39,10 @@ export default function VenueCollection() {
                                 picture={`${publicRuntimeConfig.API_URL}${item.picture}`}
                                 description={item.desc}
 
-                            />
-                        </li>
-                    ))}
-                </ul> }
+                                />
+                        </li>))}
+                    </ul> }
+                </div>
             </div>
         </div>
     );
